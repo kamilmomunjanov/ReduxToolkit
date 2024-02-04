b6fddb757a2155ab41bc27139046e7e9754f8065
Kamil <kamilmomunjanov@gmail.com>
2024-02-04 13:17:28 +0600

    Add store config and createSlice

--- src/components/User.js
+++ src/components/User.js
@@ -1,18 +1,31 @@
-import React from 'react'
-import FirstName from './FirstName'
-import LastName from './LastName'
+import React from 'react';
+import FirstName from './FirstName';
+import LastName from './LastName';
+import {useDispatch} from "react-redux";
+import {setFirstName, setLastName} from "../features/user/userSlice";
 
 const User = () => {
+    const dispatch = useDispatch()
+
+
     return (
         <div className='flex flex-col'>
             <input
                 type='text'
                 placeholder='First Name'
+                onChange={(e) => {
+                    const inputValue = e.target.value
+                    dispatch(setFirstName(inputValue))
+                }}
                 className='w-full p-1 mb-2 focus:outline-none focus:border-lime-500 focus: border-2 placeholder:text-sm'
             />
             <input
                 type='text'
                 placeholder='Second Name'
+                onChange={(e) => {
+                    const inputValue = e.target.value
+                    dispatch(setLastName(inputValue))
+                }}
                 className='w-full p-1 mb-2 focus:outline-none focus:border-lime-500 focus: border-2 placeholder:text-sm'
             />
             <div className='flex gap-20 py-5'>
